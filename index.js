8a3feb48ce1abdec9abb9645e43e40fdbc415d91
yagamy72 <138251857+yagamy72@users.noreply.github.com>
2023-08-14 18:46:46 +0000

    add devcontainer

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-mongoose.connect(process.env.MONGO_DB_URI, (err) => {
+mongoose.connect('mongodb://localhost:27017/local', (err) => {
   if (err) {
     throw err;
   }
